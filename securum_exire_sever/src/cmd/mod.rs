68325c000e03cb052df2aab147da1ea4a53c6cc8
Mayank Kumar <mayank22oct@gmail.com>
2021-06-29 19:20:25 +0530

    feat: added flags parser

--- securum_exire_sever/src/cmd/mod.rs
+++ securum_exire_sever/src/cmd/mod.rs
@@ -1,12 +1,35 @@
 use std::ops::Add;
-
+use clap::{Arg, App};
+pub fn parse_flags() -> String {
+    let matches = App::new("Securum Exire Server")
+        .version("0.1")
+        .author("Mayank Kumar <mayank22oct@gmail.com>")
+        .arg(Arg::new("deployment")
+            .short('d')
+            .long("deployment")
+            .value_name("deployment/production/staging")
+            .takes_value(true)
+            .required(true)
+            .about("takes in the type of deployment to check for in config file"))
+        .get_matches();
+    if let Some(c) = matches.value_of("deployment") {
+        if c.to_ascii_lowercase() != "development" && c.to_ascii_lowercase() != "production" && c.to_ascii_lowercase() != "staging" {
+            colour::e_red_ln!("Invalid input check --help flag!");
+            std::process::exit(1);
+        }
+        return c.to_string();
+    } else {
+        colour::e_red_ln!("error: deployment flag not supplied");
+        std::process::exit(1);
+    }
+}
 pub fn check_env() -> String {
     let home_path = std::env::var("HOME");
     if let Ok(home_path) = home_path {
         let conf_dir = home_path.add("/.securum_exire");
         let conf_dir_path = std::path::Path::new(&conf_dir);
         let conf_dir_is_present = conf_dir_path.is_dir();
-        let config_file_loc = conf_dir.clone().add("/securum.toml.example");
+        let config_file_loc = conf_dir.clone().add("/securum.toml");
         let config_file_path = std::path::Path::new(&config_file_loc);
 
         if conf_dir_is_present {
@@ -15,7 +38,7 @@ pub fn check_env() -> String {
                 return config_file_loc;
             } else {
                 colour::e_red_ln!("error: config file not found [{}]", config_file_loc);
-                colour::e_yellow_ln!("info: creating securum.toml.example");
+                colour::e_yellow_ln!("info: creating securum.toml");
                 let res =std::fs::File::create(config_file_path);
                 if res.is_err() {
                     colour::e_red_ln!("error: couldn't create {}", config_file_loc);
--- securum_exire_sever/src/main.rs
+++ securum_exire_sever/src/main.rs
@@ -12,7 +12,8 @@ mod cmd;
 #[actix_web::main]
 async fn main() {
     let config_path = cmd::check_env();
-    let conf = load_conf("production".to_string(), config_path);
+    let deployment_env = cmd::parse_flags();
+    let conf = load_conf(deployment_env, config_path);
     match start(conf).await {
         Ok(_) => {}
         Err(_) => {
